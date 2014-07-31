7da15671516377e3de8578ed7770cfa933e12543
AbstractAlgorithm <dragan.okan@gmail.com>
2014-07-31 15:44:38 +0200

    changes before merge with deployed version

--- media/js/myCode.js
+++ media/js/myCode.js
@@ -3,16 +3,9 @@ $(document).ready(function() {
         offset = 20;
 
     $(window).scroll(function() {
-        header.css
-                (
-                    'top',
-                    ($(window).scrollTop() <= 77-offset
+        var topPos  = $(window).scrollTop() <= 77-offset
                     ? (77-$(window).scrollTop())
-                    : offset) +"px"
-                );
+                    : offset;
+        header.css('top', topPos+"px");
     });
-
-    // $('#vote').click(function() {
-
-    // });
 });
